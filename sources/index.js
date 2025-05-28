4167ca2758ad818319ab84e8f8d23d9f59c40883
melisssawal <mlope16@gmail.com>
2025-05-27 20:50:21 -0400

    starting to add JS but need to reload as Vs is failing

---
+++ sources/index.js
@@ -0,0 +1,17 @@
+function generateRecipe (event) {
+    event.preventDefault ();
+
+    alert("Generating recipe...");
+}
+
+
+
+let recipeFormElement = document.querySelector("#recipe-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
+
+
+let context = "You are a master chef that has travelled the world and has a deep knowledge of recipes. Give a simple recipe using USA measurements.";
+
+
+let apiKey = "34d34bfd03ebff0892b49ada97eo706t";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}`;
