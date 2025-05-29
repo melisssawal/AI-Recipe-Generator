04ea0458de8dd2a43b618d7d9c454d2026930b4a
melisssawal <mlope16@gmail.com>
2025-05-28 21:11:10 -0400

    Fixed syntax error, changed styling/design, started JS

--- sources/index.js
+++ sources/index.js
@@ -1,15 +1,18 @@
-//function generateRecipe (event) {
-    //event.preventDefault ();
+function generateRecipe (event) {
+    event.preventDefault ();
 
-    //alert("Generating recipe...");
-//}
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.innerHTML = "";
 
-
-//let recipeFormElement = document.querySelector("#recipe-form");
-//recipeFormElement.addEventListener("submit", generateRecipe);
+}
 
 
-//let context = "You are a master chef that has travelled the world and has a deep knowledge of recipes. Give a simple recipe using USA measurements.";
+let recipeFormElement = document.querySelector("#recipe-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
 
-//let apiKey = "34d34bfd03ebff0892b49ada97eo706t";
-//let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}`;
+
+let context = "You are a master chef that has travelled the world and has a deep knowledge of recipes. Give a simple recipe using USA measurements.";
+let prompt = "Create a recipe from the entered topic";
+
+let apiKey = "34d34bfd03ebff0892b49ada97eo706t";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
