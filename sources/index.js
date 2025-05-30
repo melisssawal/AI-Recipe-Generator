39e6c0f714e6c94846a2f2eb5ce4bc840e5fcede
melisssawal <mlope16@gmail.com>
2025-05-29 21:20:39 -0400

    Fine tuned JS and Ai functionality

--- sources/index.js
+++ sources/index.js
@@ -14,11 +14,11 @@ function displayRecipe (response) {
 function generateRecipe (event) {
     event.preventDefault ();
 
-let userInstruction = document.querySelector("#user-instruction");
-let context = "You are a master baker that has travelled the world and has a deep knowledge of baking. Give a simple recipe in basic HTML. Make sure you do not include an introduction and use USA measurements. Please seperate the ingredients from the directions and start each one on it's own line. ";
-let prompt = `User instructions: Generate a recipe about bread`;
+let userInstructionInput = document.querySelector("#text-box");
+let context = "You are a master baker that has travelled the world and has a deep knowledge of baking. Give a simple recipe in basic HTML. Make sure you do not include an introduction and use USA measurements. Do not include a title. Please seperate the ingredients from the directions and start each one on it's own line. ";
+let prompt = `User instructions: Generate a recipe about ${userInstructionInput.value}`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=34d34bfd03ebff0892b49ada97eo706t`;
-
+console.log(apiUrl);
 console.log("Generating recipe");
 console.log(`prompt: ${prompt}`);
 console.log(`context: ${context}`);
